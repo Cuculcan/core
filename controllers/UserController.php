dbfbd333c27f16a76cdb5dfa92506507b9d5225b
Aleksey N <alnazarr@mail.ru>
2017-03-08 15:24:05 +1000

    user controller

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -21,40 +21,38 @@ class UserController extends BaseController
 
     protected function setActions()
     {
-        $that = $this;
-        
         //process GET request http://{my site name}/user
-        $this->get('/user', function ($urlParams) use (&$that) {
+        $this->get('/user', function ($urlParams)  {
             
-            $that->model['param'] = 'param from user controller';
-            $that->showView("UserView");
+           $this->model['param'] = 'param from user controller';
+           $this->showView("UserView");
         });
        
         //process GET request http://{my site name}/user/set_name?name=VasiliyPupkin
-        $this->get('/user/set_name', function ($urlParams) use (&$that) {
+        $this->get('/user/set_name', function ($urlParams)  {
             
-            $that->model['param'] = $that->request->getQueryParameter("name", "");           
-            $that->showView("UserView");
+           $this->model['param'] =$this->request->getQueryParameter("name", "");           
+           $this->showView("UserView");
         });
         
         //process GET request http://{my site name}/user/12345/get_name
-        $this->get('/user/{userId}/get_name', function ($urlParams) use (&$that) {
+        $this->get('/user/{userId}/get_name', function ($urlParams)  {
             
             $userId = isset($urlParams['userId']) ? $urlParams['userId']: 0;
             
-            $that->model['param'] = "User ID =".$userId;            
-            $that->showView("UserView");
+           $this->model['param'] = "User ID =".$userId;            
+           $this->showView("UserView");
         });
         
         //process POST request http://{my site name}/user/create 
         //with Form parameters name='nameOfUser'&email='email@example.com'
-        $this->post('/user/create', function ($urlParams) use (&$that) {
+        $this->post('/user/create', function ($urlParams)  {
             $userData = [];
             //get post parameters from request
-            $userData['name'] = $that->request->getQueryParameter("name", "");
-            $userData['email'] = $that->request->getQueryParameter("email", "");
+            $userData['name'] =$this->request->getQueryParameter("name", "");
+            $userData['email'] =$this->request->getQueryParameter("email", "");
             
-            $userInfo = $that->saveUser($userData);
+            $userInfo =$this->saveUser($userData);
             echo json_encode($userInfo);
         });
         
