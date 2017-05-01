ae06443a6fd40f59e3ddc9e2b0de58fafd8c0974
Lexa <aaa@bbbb.com>
2017-05-02 09:59:48 +1000

    refactor for namespaces

--- index.php
+++ index.php
@@ -12,57 +12,21 @@ ob_start();
 date_default_timezone_set("Europe/Moscow");
 
 
-//require 'src/Cuculcan/Core/autoload.php';
-//require 'Example/autoload.php';
 require 'vendor/autoload.php';
 
 require 'config/config.php';
 require 'third-party/log4php-2.3.0/php/Logger.php';
 Logger::configure('config/log4php-config.xml');
 
-// instantiate the loader
-//$loader = new \Cuculcan\Core\Psr4AutoloaderClass;
-//$loader = new \Cuculcan\Core\ClassLoader;
-//$loader->register();
-//$loader->addNamespace('Cuculcan\\Core', __DIR__."/src/Cuculcan/Core");
-//$loader->addNamespace('Example', __DIR__."/Example");
-
 $loader = new \Composer\Autoload\ClassLoader();
-      // register classes with namespaces
+// register classes with namespaces
 $loader->add('Cuculcan\Core', __DIR__."/src");
 $loader->add('Example',      __DIR__.'/');
 
 // activate the autoloader
 $loader->register();
 
-// to enable searching the include path (eg. for PEAR packages)
-//$loader->setUseIncludePath(true);
-
-
 use Cuculcan\Core\Application;
-use Cuculcan\Core\Errors\BadRequestException;
-
-//use Example\Services\MyAop;
-use Example\Services\ApplicationAspectKernel;
-use Example\Services\aopClass;
-
-
-
-
-$applicationAspectKernel = ApplicationAspectKernel::getInstance();
-$applicationAspectKernel->init(array(
-    'debug' => true, // Use 'false' for production mode
-    // Cache directory
-    'cacheDir' => $_SERVER["DOCUMENT_ROOT"] . '/cache/', // Adjust this path if needed
-    // Include paths restricts the directories where aspects should be applied, or empty for all source files
-    'includePaths' => array(
-        $_SERVER["DOCUMENT_ROOT"]. '/src/Cuculcan/Core',
-        $_SERVER["DOCUMENT_ROOT"]. '/Example/Services'
-    )
-));
-
-$aop = new aopClass();
-$aop->doAop("bzzzz");
 
 (new Application("Example"))->run();
 
--- src/Cuculcan/Core/Application.php
+++ src/Cuculcan/Core/Application.php
@@ -10,8 +10,6 @@ class Application {
     }
 
     public function run() {
-        echo "pew-pew!!!!";
-       // return;
         //prepare request data
         $request = new Request();
        
