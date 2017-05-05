b738b7c94c50930bb82294ac22070ea56f13324d
Lexa <aaa@bbbb.com>
2017-05-05 15:56:42 +1000

    formatting

--- src/Example/Controllers/MainController.php
+++ src/Example/Controllers/MainController.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Controller for process site root url
  * 
@@ -16,22 +17,25 @@ use Example\Controllers\Base\BaseController;
 //include 'controllers/base/BaseController.php';
 class MainController extends BaseController
 {
-    public function __construct($request, $namespce) {
+
+    public function __construct($request, $namespce)
+    {
         parent::__construct($request, $namespce);
         $this->log = \Logger::getLogger(__CLASS__);
     }
 
-    protected function setActions() {
-      
-        $this->get('/main', function($urlParams){
+    protected function setActions()
+    {
+
+        $this->get('/main', function($urlParams) {
 
             $this->prepareDataForMainPage();
             $this->showView("MainView");
         });
-        
     }
-    
-    private function prepareDataForMainPage(){
+
+    private function prepareDataForMainPage()
+    {
         //echo "prepare data for main page!!";
     }
 
